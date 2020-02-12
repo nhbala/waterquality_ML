dd668515081114a39037338557468ca934ba128b
nhbala <nb378@cornell.edu>
2020-02-12 17:16:04 -0500

    api weather working

--- get_weather.py
+++ get_weather.py
@@ -1,21 +1,11 @@
 import requests, json
 
 
-def five_day_weather(city):
-    api_key = "7dee77df9de9d65a1de03be656176254"
-    country = "HN"
-    complete_url = "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "," + country + "uk&appid=" + api_key
+def test_api():
+    api_key = "56132dfa8c3dc4b5cb47372c76f9f618"
+    complete_url = "https://api.darksky.net/forecast/" + api_key + "/" + "14.116667" + "," + "-86.866667" + "," + "1581545399?exclude=currently,flags"
     response = requests.get(complete_url)
     data_return = response.json()
-    weather_data = data_return['list']
-    index = 2
-    rain_index = []
-    while index < len(weather_data):
-        weather_dic = (weather_data[index])['weather']
-        print(weather_dic[0]['main'] )
-        if weather_dic[0]['main'] == 'Rain':
-            rain_index.append(True)
-        else:
-            rain_index.append(False)
-        index += 8
-    return rain_index
+    print(data_return)
+if __name__=="__main__":
+    test_api()
