ec6d1bf3d03026f95ca13317f30ca74f51db8273
nhbala <nb378@cornell.edu>
2020-02-19 16:06:56 -0500

    fixed api issue

--- get_weather.py
+++ get_weather.py
@@ -30,18 +30,21 @@ Primary class for weather api
 class Weather:
     """
     """
-    api_key = "56132dfa8c3dc4b5cb47372c76f9f618"
+
 
     def __init__(self):
         self.db = {"moroceli": Plant("Moroceli", (14.116667,-86.866667))}
+        self.api_key = "56132dfa8c3dc4b5cb47372c76f9f618"
 
     def general_call(self, plant_name, when):
-        assert type(when) == datetime
+        assert type(when) == datetime.datetime
 
         plant_name = plant_name.lower()
         plant_coords = self.db[plant_name].get_coords()
-        unixtime - time.mktime(when.timetuple())
-        complete_url = "https://api.darksky.net/forecast/"+api_key+"/"+str(plant_coords[0])+","+str(plant_coords[1])+","+str(unixtime)+ "?exclude=currently,flags"
+        unixtime = time.mktime(when.timetuple())
+        print(int(unixtime))
+        complete_url = "https://api.darksky.net/forecast/"+self.api_key+"/"+str(plant_coords[0])+","+str(plant_coords[1])+","+str(int(unixtime))+ "?exclude=currently,flags"
+        print(complete_url)
         response = requests.get(complete_url)
         return response.json()
 
