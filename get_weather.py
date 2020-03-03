0c40409323dad7b61be7dc204102d358b1cf8623
wg253 <wg253@cornell.edu>
2020-03-03 16:21:43 -0500

    Changed general call to get coords of watershed instead of plant itself

--- get_weather.py
+++ get_weather.py
@@ -2,9 +2,14 @@ import requests, json
 import time
 import datetime
 
-
+"""
+This class represents an instance of a water treament plant and contains information
+about it, specifically its name, coordinates, and the coordinates of its watershed,
+defaulted to 0,0 if not inputted.
+"""
 class Plant:
     """
+    Constructs an object of class Plant with name, coordinates, and coordinates of watershed
     """
     def __init__(self, name, coords, watershed = (0.0,0.0)):
         assert type(name) == str
@@ -24,31 +29,39 @@ class Plant:
     def get_coords(self):
         return self.coords
 
+    def get_watershed(self):
+        return self.watershed
+
 """
 Primary class for weather api
 """
 class Weather:
     """
+    Constructs an object of class Weather through which useful methods are called
     """
-
-
     def __init__(self):
         self.db = {"moroceli": Plant("Moroceli", (14.116667,-86.866667))}
         self.api_key = "56132dfa8c3dc4b5cb47372c76f9f618"
 
+    """
+    Returns the json containing weather information at specified plant's watershed and time
+    """
     def general_call(self, plant_name, when):
         assert type(when) == datetime.datetime
 
         plant_name = plant_name.lower()
-        plant_coords = self.db[plant_name].get_coords()
+        plant_coords = self.db[plant_name].get_watershed()
         unixtime = time.mktime(when.timetuple())
-        print(int(unixtime))
         complete_url = "https://api.darksky.net/forecast/"+self.api_key+"/"+str(plant_coords[0])+","+str(plant_coords[1])+","+str(int(unixtime))+ "?exclude=currently,flags"
-        print(complete_url)
         response = requests.get(complete_url)
+        print(type(response))
         return response.json()
+
+    def spec_call(self, param, plant_name, when):
+        big_dict = self.general_call(plant_name, when)
 
 
 if __name__=="__main__":
     weather = Weather()
     print(weather.general_call("moroceli", datetime.datetime(1999,6,7)))
+    #weather.spec_call("precipitation", "moroceli", datetime.datetime(1999,6,7))
