907baf8665a8eca74b8c4e3f4677892be526dd71
nhbala <nb378@cornell.edu>
2020-03-04 16:55:53 -0500

    added some cluster stuff

--- get_weather.py
+++ get_weather.py
@@ -1,18 +1,14 @@
 import requests, json
 import time
-<<<<<<< HEAD
 import firebase_testing
 from datetime import datetime, timedelta
-
-=======
-import datetime
+from sklearn import cluster
 
 """
 This class represents an instance of a water treament plant and contains information
 about it, specifically its name, coordinates, and the coordinates of its watershed,
 defaulted to 0,0 if not inputted.
 """
->>>>>>> 0c40409323dad7b61be7dc204102d358b1cf8623
 class Plant:
     """
     Constructs an object of class Plant with name, coordinates, and coordinates of watershed
@@ -46,7 +42,7 @@ class Weather:
     Constructs an object of class Weather through which useful methods are called
     """
     def __init__(self):
-        self.db = {"moroceli": Plant("Moroceli", (14.148560659841669,-86.73088073730469))}
+        self.db = {"moroceli": Plant("Moroceli", (14.116667,-86.866667))}
         self.api_key = "56132dfa8c3dc4b5cb47372c76f9f618"
 
     """
@@ -56,7 +52,7 @@ class Weather:
         assert type(when) == datetime
 
         plant_name = plant_name.lower()
-        plant_coords = self.db[plant_name].get_watershed()
+        plant_coords = self.db[plant_name].get_coords()
         unixtime = time.mktime(when.timetuple())
         complete_url = "https://api.darksky.net/forecast/"+self.api_key+"/"+str(plant_coords[0])+","+str(plant_coords[1])+","+str(int(unixtime))+ "?exclude=currently,flags"
         response = requests.get(complete_url)
@@ -69,26 +65,32 @@ class Weather:
 
 if __name__=="__main__":
     weather = Weather()
-<<<<<<< HEAD
     data = firebase_testing.get_graph_data()
     moroceli_data = data['Moroceli']
+    data_matrix = []
+    total = 0
     for data_point in moroceli_data:
-        date = data_point['timeFinished']
-        t_index = date.index("T")
-        final_date = date[:t_index]
-        final_time = date[t_index + 1: -1]
+        date_1 = data_point['timeFinished']
+        t_index = date_1.index("T")
+        final_date = date_1[:t_index]
+        final_time = date_1[t_index + 1: -1]
         final_total_date = final_date + " " + final_time
         datetime_object = datetime.strptime(final_total_date, '%Y-%m-%d %H:%M:%S.%f')
-        curr_weather = weather.general_call("moroceli", datetime_object - timedelta(hours=2))
-        print(curr_weather)
+        curr_weather = weather.general_call("moroceli", datetime_object - timedelta(hours=1))
         first_point = curr_weather['hourly']['data'][0]
         #how to get data?
         curr_percep_intensity = first_point.get('precipIntensity', -1)
-        curr_pressure = first_point.get('pressure', 1)
-        print(curr_percep_intensity)
-        print(curr_pressure)
-        break
-=======
-    print(weather.general_call("moroceli", datetime.datetime(1999,6,7)))
-    #weather.spec_call("precipitation", "moroceli", datetime.datetime(1999,6,7))
->>>>>>> 0c40409323dad7b61be7dc204102d358b1cf8623
+        curr_pressure = first_point.get('pressure', -1)
+        curr_wind_speed = first_point.get("windSpeed", -1)
+        curr_temp = first_point.get("temperature", -1)
+        curr_raw_turb = data_point.get("rawWaterTurbidity", -1)
+        curr_settle_turb = data_point.get("settledWaterTurbidity", -1)
+        curr_point = [curr_percep_intensity, curr_pressure, curr_wind_speed, curr_temp, curr_raw_turb, curr_settle_turb]
+        print(curr_point)
+        data_matrix.append(curr_point)
+        total += 1
+        if total > 50:
+            break
+kmeans = cluster.KMeans(random_state=0).fit(data_matrix)
+print(data_matrix)
+print(kmeans.labels_)
